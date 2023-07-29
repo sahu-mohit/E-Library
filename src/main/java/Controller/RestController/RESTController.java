60416f2bc415e5a3ffa4b3d3874a125c840532b9
Govind Kumar <gk4142434@gmail.com>
2023-07-30 01:21:30 +0530

    govind

--- src/main/java/Controller/RestController/RESTController.java
+++ src/main/java/Controller/RestController/RESTController.java
@@ -1,14 +1,19 @@
 package Controller.RestController;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+
+@Controller
+@RequestMapping("/rest")
 public class RESTController {
 
-    @GetMapping("/")
+    @RequestMapping("/index")
     public String gettest(){
-        System.out.println("hgjmhgj");
-        return "hg";
+      
+        return "home";
     }
 }
